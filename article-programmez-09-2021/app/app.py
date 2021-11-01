02837583e962838d69ea28997e3f1074875e3ffc
Benoit Petit <bpetit@nce.re>
2021-11-01 08:50:56 +0100

    last minute update not pushed

--- article-programmez-09-2021/app/app.py
+++ article-programmez-09-2021/app/app.py
@@ -4,35 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def fibo():
-    response = "\n la suite fibonacci est : "
-    response += str(fibonacci(30))
-    return response
-
-def fibonacci(n):
-    if(n <= 1):
-        return n
-    else:
-        return (fibonacci(n-1) + fibonacci(n-2))
-    response = ""
-    for i in range(n):
-        response += str(fibonacci(i))
-    return response+", "
-
 #@app.route("/")
 #def fibo():
-#    nterms = 30
-#    n1 = 0
-#    n2 = 1
-#
-#    response = "\n la suite fibonacci est : {}, {},".format(n1,n2)
-#
-#    for i in range(2, nterms):
-#      suivant = n1 + n2
-#      response += "{},".format(suivant)
-#
-#      n1 = n2
-#      n2 = suivant
-#
+#    response = "\n la suite fibonacci est : "
+#    response += str(fibonacci(30))
 #    return response
+#
+#def fibonacci(n):
+#    if(n <= 1):
+#        return n
+#    else:
+#        return (fibonacci(n-1) + fibonacci(n-2))
+#    response = ""
+#    for i in range(n):
+#        response += str(fibonacci(i))
+#    return response+", "
+
+@app.route("/")
+def fibo():
+    nterms = 30
+    n1 = 0
+    n2 = 1
+
+    response = "\n la suite fibonacci est : {}, {},".format(n1,n2)
+
+    for i in range(2, nterms):
+      suivant = n1 + n2
+      response += "{},".format(suivant)
+
+      n1 = n2
+      n2 = suivant
+
+    return response
