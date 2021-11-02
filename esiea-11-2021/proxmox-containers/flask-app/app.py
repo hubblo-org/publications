f2320a6ac75a62d2fbc740036a53d228561d2486
Benoit Petit <bpetit@nce.re>
2021-11-02 16:07:58 +0100

    fixed app

--- esiea-11-2021/proxmox-containers/flask-app/app.py
+++ esiea-11-2021/proxmox-containers/flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def main():
-    return "YOU DID IT ! I'm running on host : {}\n Client is : {}\n".format(request.environ['SERVER_ADDR'], request.remote_addr)
+    return "YOU DID IT ! Client is : {}\n".format(request.remote_addr)
 
 if __name__ == '__main__':
     app.run()
