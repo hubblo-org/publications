61baa3bcf49ef8dc997ff0909b4300ea5fd0f699
Benoit Petit <bpetit@nce.re>
2021-11-02 09:43:18 +0100

    improved getimages

---
+++ esiea-11-2021/getimages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+QCOW_IMAGE="myimage.qcow2.gz"
+OVA_IMAGE="proxmox.ova"
+MD5SUM="https://raw.githubusercontent.com/hubblo-org/publications/master/esiea-11-2021/md5sum"
+
+curl ${MD5SUM} -s -o md5sum
+
+curl https://sync.nce.re/index.php/s/2EqzdgqxNA3pWn3/download -# -o ${QCOW_IMAGE}
+if [ $? -eq 0 ]; then
+  res=$(md5sum ${QCOW_IMAGE}); grep ${res} md5sum && gunzip ${QCOW_IMAGE} && echo "${QCOW_IMAGE} downloaded, sane, then gunzipped."
+else
+  echo "Download of ${QCOW_IMAGE} failed !"
+fi
+curl https://sync.nce.re/index.php/s/Ap9eWykmnC49Cnx/download -# -o ${OVA_IMAGE}
+if [ $? -eq 0 ]; then
+  res=$(md5sum ${OVA_IMAGE}); grep ${res} md5sum && echo "${OVA_IMAGE}, sane, downloaded."
+else
+  echo "Download of ${OVA_IMAGE} failed !"
+fi
